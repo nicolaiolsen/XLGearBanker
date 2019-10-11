0c63616ebadc47bb55933f1a3fcc768ae70ee35b
XLOlsen <theolsenbanden@gmail.com>
2019-10-11 19:06:17 +0200

    reworked banking module

--- XLGearBanker/XLGB_Banking.lua
+++ XLGearBanker/XLGB_Banking.lua
@@ -10,12 +10,7 @@
 --Namespace
 XLGB_Banking = {}
 
-function XLGB_Banking:Initialize()
-  self.bankOpen = IsBankOpen()
-  self.debug = true
-  EVENT_MANAGER:RegisterForEvent(self.name, EVENT_OPEN_BANK, self.OnBankOpenEvent)
-  EVENT_MANAGER:RegisterForEvent(self.name, EVENT_CLOSE_BANK, self.OnBankCloseEvent)
-end
+local ITEM_NOT_IN_BAG = -1
 
 function XLGB_Banking.OnBankOpenEvent(event, bankBag)
   if not XLGB_Banking.bankOpen then
@@ -31,64 +26,15 @@ function XLGB_Banking.OnBankCloseEvent(event)
   end
 end
 
---[[
-  function depositGear
-  Input:
-
-  Output:
-]]--
-function XLGB_Banking.depositGear(gearSetNumber)
-  XLGB_Banking:MoveGear(BAG_BACKPACK, BAG_BANK, gearSetNumber)
-end
-
---[[
-  function withdrawGear
-  Input:
-
-  Output:
-]]--
-function XLGB_Banking.withdrawGear(gearSetNumber)
-  XLGB_Banking.moveGear(BAG_BANK, BAG_BACKPACK, gearSetNumber)
-end
-
-function XLGB_Banking.moveGear(sourceBag, targetBag, gearSetNumber)
-  easyDebug("Moving gearSet #", gearSetNumber)
-  if not XLGB_Banking.bankOpen then
-    easyDebug("Bank is not open!")
-    return
-  else
-    -- retrieve list of item ids (gearSet) related to the gearSetNumber
-    local gearSet = GearSet:GetGearSet(gearSetNumber)
-    local availableBagSpaces = XLGB_Banking:GetAvailableBagSpaces(targetBag)
-    --Move each item of the specified gearset from sourceBag to targetBag
-    for _, item in ipairs(gearSet) do
-      XLGB_Banking:MoveItem(sourceBag, targetBag, item, availableBagSpaces)
-    end
-  end
-end
-
-
-function XLGB_Banking:MoveItem(sourceBag, targetBag, item, availableBagSpaces)
-  easyDebug("Moving item", item)
-  --local itemIndex = XLGB_Banking:FindItemIndexInBag(sourceBag, item)
-  local itemIndex = item
-  local moveSuccesful = CallSecureProtected("RequestMoveItem", sourceBag, itemIndex, targetBag, availableBagSpaces[#availableBagSpaces], 1)
-
-  if moveSuccesful then
-    easyDebug("Item move: Success!")
-    table.remove(availableBagSpaces)
-  else
-    easyDebug("Item move: Failure!")
-  end
-end
-
-function XLGB_Banking:FindItemIndexInBag(bag, itemID)
+local function findItemIndexInBag(bag, itemLink)
+  local item_index = -1
   for i = 0, GetBagSize(bag) do
-    if GetItemUniqueID(bag, i) == itemID then
-      return i
+    if GetItemLink(bag, i) == itemLink then
+      item_index = i
+      return item_index
     end
   end
-  return -1
+  return item_index
 end
 
 --[[
@@ -102,15 +48,71 @@ end
   Output:
     availableBagSpaces = Lua table containing indices of empty bag slots.
 ]]--
-function XLGB_Banking:GetAvailableBagSpaces(bag)
+local function getAvailableBagSpaces(bag)
   easyDebug("Finding available bagspaces in bag: ", bag )
   local availableBagSpaces = {}
 
-  for i = FindFirstEmptySlotInBag(bag), GetBagSize(bag)-1 do
+  for i = 0, GetBagSize(bag)-1 do
     if GetItemName(bag, i) == "" then
       table.insert(availableBagSpaces, #availableBagSpaces, i)
     end
   end
   easyDebug("Found ", #availableBagSpaces, " available spaces in bag.")
   return availableBagSpaces
+end
+
+local function moveItem(sourceBag, targetBag, itemLink, availableBagSpaces)
+  easyDebug("Moving item", itemLink)
+  local itemIndex = findItemIndexInBag(sourceBag, itemLink)
+  local moveSuccesful = false
+  if (itemIndex ~= ITEM_NOT_IN_BAG) then
+    moveSuccesful = CallSecureProtected("RequestMoveItem", sourceBag, itemIndex, targetBag, availableBagSpaces[#availableBagSpaces], 1)
+  end
+
+  if moveSuccesful then
+    easyDebug("Item move: Success!")
+    table.remove(availableBagSpaces)
+  end
+end
+
+local function moveGear(sourceBag, targetBag, gearSetNumber)
+  easyDebug("Moving gearSet #", gearSetNumber)
+  if not XLGB_Banking.bankOpen then
+    easyDebug("Bank is not open!")
+    return
+  else
+    -- retrieve list of item ids (gearSet) related to the gearSetNumber
+    local gearSet = GearSet:GetGearSet(gearSetNumber)
+    local availableBagSpaces = getAvailableBagSpaces(targetBag)
+    --Move each item of the specified gearset from sourceBag to targetBag
+    for _, item in pairs(gearSet.items) do
+      moveItem(sourceBag, targetBag, item.link, availableBagSpaces)
+    end
+  end
+end
+--[[
+  function depositGear
+  Input:
+
+  Output:
+]]--
+function XLGB_Banking:DepositGear(gearSetNumber)
+  moveGear(BAG_BACKPACK, BAG_BANK, gearSetNumber)
+end
+
+--[[
+  function withdrawGear
+  Input:
+
+  Output:
+]]--
+function XLGB_Banking:WithdrawGear(gearSetNumber)
+  moveGear(BAG_BANK, BAG_BACKPACK, gearSetNumber)
+end
+
+function XLGB_Banking:Initialize()
+  self.bankOpen = IsBankOpen()
+  self.debug = true
+  EVENT_MANAGER:RegisterForEvent(self.name, EVENT_OPEN_BANK, self.OnBankOpenEvent)
+  EVENT_MANAGER:RegisterForEvent(self.name, EVENT_CLOSE_BANK, self.OnBankCloseEvent)
 end
