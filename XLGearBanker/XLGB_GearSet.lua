721700844411f606877193a47715c4bc727e672c
XLOlsen <theolsenbanden@gmail.com>
2019-10-17 11:44:53 +0200

    trying deep_copy function

--- XLGearBanker/XLGB_GearSet.lua
+++ XLGearBanker/XLGB_GearSet.lua
@@ -21,6 +21,19 @@ function XLGB_GearSet:ValidGearSetNumber(gearSetNumber, totalGearSets)
   end
 end
 
+-- credit: https://gist.github.com/tylerneylon/81333721109155b2d244
+--
+local function copy(obj, seen)
+  if type(obj) ~= 'table' then return obj end
+  if seen and seen[obj] then return seen[obj] end
+  local s = seen or {}
+  local res = setmetatable({}, getmetatable(obj))
+  s[obj] = res
+  for k, v in pairs(obj) do res[copy(k, s)] = copy(v, s) end
+  return res
+end
+
+
 function XLGB_GearSet:ValidGearSetName(gearSetName)
   if gearSetName == nil
   or gearSetName == "" then 
@@ -100,7 +113,7 @@ function XLGB_GearSet:AddItemToGearSet(itemLink, itemID, gearSetNumber)
 end
 
 function XLGB_GearSet:RemoveItemFromGearSet(itemLink, itemID, gearSetNumber)
-  local gearSet = XLGB_GearSet:GetGearSet(gearSetNumber)
+  local gearSet = copy(XLGB_GearSet:GetGearSet(gearSetNumber))
   local gearSetName = gearSet.name
 
   for i, item in pairs(gearSet.items) do
