7cd8bca763fa08e9e23e8032eac5eea7a09abe73
XLOlsen <theolsenbanden@gmail.com>
2019-12-10 01:49:17 +0100

    ui

--- XLGearBanker/XLGB_UI.lua
+++ XLGearBanker/XLGB_UI.lua
@@ -223,7 +223,7 @@ end
 function XLGB_UI:UpdateScrollList(gearSetNumber)
   local gearSet = XLGB_GearSet:GetGearSet(gearSetNumber)
   local scrollData = ZO_ScrollList_GetDataList(XLGB_Window_Control_ListView.scrollList)
-  if scrollData then ZO_ScrollList_Clear(scrollData) end
+  if not (scrollData == nil) then ZO_ScrollList_Clear(scrollData) end
   for i, item in pairs(gearSet.items) do
       local dataEntry = ZO_ScrollList_CreateDataEntry(XLGB_Constants.ITEM_ROW, {
         itemName = item.name,
