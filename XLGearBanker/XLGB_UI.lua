14b403c36e2022af8969f3610db8c6fa63735602
XLOlsen <theolsenbanden@gmail.com>
2019-12-12 05:13:52 +0100

    debugging

--- XLGearBanker/XLGB_UI.lua
+++ XLGearBanker/XLGB_UI.lua
@@ -191,6 +191,7 @@ function XLGB_UI:HideUI()
 end
 
 function XLGB_UI:RemoveItem(removeItemControl)
+  d(removeItemControl:GetText())
   d("1")
   itemControl = removeItemControl:GetParent()
   d("2")
