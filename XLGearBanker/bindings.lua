6d9acc79259cb5dc9cd6aad90a2fdde3a122e2f5
XLOlsen <theolsenbanden@gmail.com>
2019-12-15 08:15:33 +0100

    bindings-lua update toggle ui button

--- XLGearBanker/bindings.lua
+++ XLGearBanker/bindings.lua
@@ -1,8 +1,6 @@
 -- this puts the items in the bind list, calls are located in bindings.xml
-ZO_CreateStringId("SI_BINDING_NAME_DEPOSIT_ASSIGNED_STORAGE_ITEMS",
-"Deposit Assigned")
-ZO_CreateStringId("SI_BINDING_NAME_WITHDRAW_ASSIGNED_STORAGE_ITEMS",
-"Withdraw Assigned")
+ZO_CreateStringId("SI_BINDING_NAME_TOGGLE_XLGB_UI",
+"Toggle XLGB UI")
 
 ZO_CreateStringId("SI_BINDING_NAME_DEPOSIT_GEAR_SET_1",
 "Deposit Gear Set 1")
