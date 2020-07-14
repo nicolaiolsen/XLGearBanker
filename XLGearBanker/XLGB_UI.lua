a085e780d3eaad15272ed6f7ad17c46152c1266f
Nicolai Olsen <201508368@post.au.dk>
2020-07-14 02:08:59 +0200

    SelectSet on init

--- XLGearBanker/XLGB_UI.lua
+++ XLGearBanker/XLGB_UI.lua
@@ -388,6 +388,7 @@ function XLGB_UI:InitializeDropdown()
   XLGB_UI.set = XLGB_SetWindow_SetRow_Set
   XLGB_UI.set.dropdown = ZO_ComboBox_ObjectFromContainer(XLGB_UI.set)
   XLGB_UI:UpdateSetDropdown()
+  XLGB_UI:SelectSet(XLGearBanker.displayingSet)
 end
 
 function XLGB_UI:UpdateScrollList()
