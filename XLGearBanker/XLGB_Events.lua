351766c285d3e2b25957b22b1cde67cfc8e26e2d
Nicolai Olsen <201508368@post.au.dk>
2020-07-14 06:48:54 +0200

    Events refactor

--- XLGearBanker/XLGB_Events.lua
+++ XLGearBanker/XLGB_Events.lua
@@ -1,10 +1,10 @@
 XLGB_Events = {}
 
 function XLGB_Events:OnGearSetItemAdd(gearSetBefore, gearSetAfter)
-  XLGB_UI:UpdateScrollList()
+  XLGB_UI:UpdateSetScrollList()
 end
 
 function XLGB_Events:OnGearSetItemRemove(gearSetBefore, gearSetAfter)
-  XLGB_UI:UpdateScrollList()
+  XLGB_UI:UpdateSetScrollList()
 end
 
