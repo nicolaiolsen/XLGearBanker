3220c5afd8f780bd2d7fdc81c052d4579c3569ff
Nicolai Olsen <201508368@post.au.dk>
2020-07-18 19:09:00 +0200

    playing with event

--- XLGearBanker/XLGearBanker.lua
+++ XLGearBanker/XLGearBanker.lua
@@ -59,7 +59,7 @@ SLASH_COMMANDS["/xlgb_event"] = function (argsv)
     EVENT_MANAGER:UnregisterForEvent(XLGearBanker.name .. "InventoryChanged", EVENT_INVENTORY_SINGLE_SLOT_UPDATE)
   else
     EVENT_MANAGER:RegisterForEvent(XLGearBanker.name .. "InventoryChanged", EVENT_INVENTORY_SINGLE_SLOT_UPDATE, _onInventoryChanged)
-    EVENT_MANAGER:AddFilterForEvent(XLGearBanker.name .. "InventoryChanged", EVENT_INVENTORY_SINGLE_SLOT_UPDATE, REGISTER_FILTER_IS_NEW_ITEM, true)
+    EVENT_MANAGER:AddFilterForEvent(XLGearBanker.name .. "InventoryChanged", EVENT_INVENTORY_SINGLE_SLOT_UPDATE, REGISTER_FILTER_IS_NEW_ITEM, false)
     EVENT_MANAGER:AddFilterForEvent(XLGearBanker.name .. "InventoryChanged", EVENT_INVENTORY_SINGLE_SLOT_UPDATE, REGISTER_FILTER_BAG_ID, BAG_BACKPACK)
     EVENT_MANAGER:AddFilterForEvent(XLGearBanker.name .. "InventoryChanged", EVENT_INVENTORY_SINGLE_SLOT_UPDATE, REGISTER_FILTER_INVENTORY_UPDATE_REASON, INVENTORY_UPDATE_REASON_DEFAULT)
   end
